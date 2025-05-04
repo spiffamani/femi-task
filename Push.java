64cc2bb07169cda052f62d5a77743297c9cf7263
spiffamani <cjdaveeds@gmail.com>
2025-05-04 19:55:08 +0100

    '

---
+++ Push.java
@@ -0,0 +1,19 @@
+public class Push{
+public static Integer findmultiple(int number){
+int result = 0;
+int count;
+for(count = 5; count<= number; count++){
+result =number * count ;
+}
+return result;
+}
+}
+public static Integer primenumber(int number){
+int display;
+for(display = 3; display <= number; display++)
+if (number % display = 0 || number % 2 == 0)
+number = true;
+else
+number = false;
+return number;
+}
