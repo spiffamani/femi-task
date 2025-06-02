e4f24d218493154b551f1089799b33978c9c5709
Godwin Chijioke <cjdaveeds@gmail.com>
2025-06-02 17:26:18 +0100

    Add files via upload

---
+++ RandomNumberGenerator.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class RandomNumberGenerator {
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+int count = 0;
+int total = 0 ;
+int counter = 0;
+int maximum = 0;
+int minimum = 0;
+ 
+for(int index = 0; index <= 10; index++){
+
+
+ maximum = (int)Math.random() * 20;
+ minimum = (int)Math.random() * 20;
+total = maximum - minimum
+
+if (maximum > minimum){
+ 
+System.out.printf("what is %d -%d", maximum , minimum);
+
+int usernumber = input.nextInt();
+
+if usernumber ==  total);
+
+}
+while(total <= 2){
+
+if( total  == usernumber){
+
+count++ ;
+
+System.out.printf(" correst answer gotten is ",  count);
+
+}
+
+
+else if ( total  != usernumber){
+
+counter++ ;
+
+System.out.printf(" failed answer gotten is ",  counter);
+ }
+ 
+}
+}
+}
+}
+
+
+
+
